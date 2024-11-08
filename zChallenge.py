5ba7e673f2f64be0aa5e68a8e8876789fa612a54
Junming1120 <159005069+Junming1120@users.noreply.github.com>
2024-11-08 09:43:40 -0800

    1

--- zChallenge.py
+++ zChallenge.py
@@ -10,5 +10,5 @@ for i in range(40):
         x.append(random.randint(1,10) + random.randint(1,10)/10)
 
 print(x)
-
+#  x are 40 random number from 1,10,and x have half chance to be a integer and half chance to be a float.
 
